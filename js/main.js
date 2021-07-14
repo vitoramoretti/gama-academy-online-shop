20e3b26af83596d73dad1aa8787210042a255b38
Vitor Amoretti <vitoramoretti@AmorUnas-Mac-mini.local>
2021-07-14 01:08:48 -0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const save = document.querySelector('.save');
+const form = document.querySelector('form');
+const getName = document.querySelector('#email-input');
+const submitBtn = document.querySelector('#submit');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+})
+ 
+
+submitBtn.addEventListener('click', function() {
+    localStorage.setItem('email', getName.value);
+    document.location.href='confirm.html';
+})
